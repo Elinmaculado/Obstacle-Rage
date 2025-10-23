2f9de15fcc324484cca70fef67bb279e5219f6b3
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2025-10-23 12:44:25 -0600

    Bakushin

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
 
     public float speed = 5;
+    private Vector3 _playerVelocity;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.8f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,14 +24,29 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
+        
+        if(_controller.isGrounded)
+            Debug.Log("Character is grounded");
+        else
+            Debug.Log("Character NOT grounded");
+            
     }
 
     void MovePlayer()
     {
+
+        if (_controller.isGrounded && _actions.Player.Jump.WasPerformedThisFrame())
+        {
+            Debug.Log("Jump");
+            _playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+        
+        _playerVelocity.y += gravity * Time.deltaTime;
         
         _movementInput = _actions.Player.Move.ReadValue<Vector2>();
         Vector3 movement = new Vector3(_movementInput.x, 0, _movementInput.y);
-        _controller.Move(movement * Time.deltaTime * speed);
+        Vector3 finalMove = (movement * speed) + (_playerVelocity.y * Vector3.up);
+        _controller.Move(finalMove * Time.deltaTime * speed);
     }
     
 }

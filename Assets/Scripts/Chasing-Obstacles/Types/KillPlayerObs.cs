d349a249636a5acdf611a51d459aa381bce226fe
LuigiTan <gibran.garcia@uniat.edu.mx>
2025-11-06 00:32:48 -0600

    Preliminary Map

--- Assets/Scripts/Chasing-Obstacles/Types/KillPlayerObs.cs
+++ Assets/Scripts/Chasing-Obstacles/Types/KillPlayerObs.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "KillPlayerObs", menuName = "ChasingObstacles/KillPlayer")]
 public class KillPlayerObs : Base_ChasingObs
@@ -11,7 +12,8 @@ public class KillPlayerObs : Base_ChasingObs
         contactTime += deltaTime;
         if (contactTime >= 1.5f)
         {
-            GameObject.Destroy(player);
+            //GameObject.Destroy(player);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             contactTime = 0f;
         }
     }
